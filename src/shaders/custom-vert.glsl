490ebacdae8bfb30f56e46e2f1bd4d3b1c898754
siwel-cg <lghrist@seas.upenn.edu>
2024-09-09 04:14:50 -0400

    vertex deform v1

--- src/shaders/custom-vert.glsl
+++ src/shaders/custom-vert.glsl
@@ -22,9 +22,14 @@ void main()
     fs_Time = u_Time;                     
     mat3 invTranspose = mat3(u_ModelInvTr);
     fs_Nor = vec4(invTranspose * vec3(vs_Nor), 0);
+    float a = sin(u_Time * 0.005) * 3.141592 * 0.4;
+    if (vs_Pos.y > 0.0) {
+        a = a * -1.0;
+    }
+    mat4 rot = mat4(vec4(cos(a), 0, -sin(a), 0), vec4(0, 1, 0, 0), vec4(sin(a), 0, cos(a), 0), vec4(0,0,0,1));
 
     vec4 modelposition = u_Model * vs_Pos;
     fs_LightVec = lightPos - modelposition;
 
-    gl_Position = u_ViewProj * modelposition;
+    gl_Position = u_ViewProj * rot * modelposition;
 }
